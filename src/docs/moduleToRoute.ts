fd28c567521941eb510117a28b27957172ff4643
bigbigking <2845758942@qq.com>
2022-06-28 17:14:03 +0800

    pref

--- src/docs/moduleToRoute.ts
+++ src/docs/moduleToRoute.ts
@@ -17,40 +17,18 @@ export default function moduleToRoute(modules:Record<string, {[key: string]: any
       //子级路由信息
       const childName = key.split('/')[3].slice(0,-3)  //截取到文件名
       const childPath = childName
-      if(routes.length > 0){
-        let index = routes.findIndex(route => route.name === name)
+      let index = routes.findIndex(route => route.name === name)
+      if(routes.length > 0 && index > -1){
         //检查父级路由是否存在
-        if(index > -1){
-          //如果存在则添加子路由
-          routes[index].children?.push({
-            path: childPath,
-            name: `${name}-${childName}`,
-            component: module,
-            meta: {
-              title: `${name}-${childName}`
-            }
-          })
-        }else{
-          //如果不存在则添加父级路由和当前为子路由
-          routes.push({
-            path: path,
-            name: name,
-            component: Layout,
-            meta: {
-              title: name
-            },
-            children: [
-              {
-                path: childPath,
-                name: `${name}-${childName}`,
-                component: module,
-                meta: {
-                  title: `${name}-${childName}`
-                }
-              }
-            ]
-          })
-        }
+        //如果存在则添加子路由
+        routes[index].children?.push({
+          path: childPath,
+          name: `${name}-${childName}`,
+          component: module,
+          meta: {
+            title: `${name}-${childName}`
+          }
+        })
       }else{
         //当前还没有任何路由
         //push一个路由做为父级路由并添加当前为子级路由
@@ -75,5 +53,6 @@ export default function moduleToRoute(modules:Record<string, {[key: string]: any
       }
     }
   }
+  console.log('routes',routes)
   return routes
 }
