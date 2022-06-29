dcfe002ce19dce33c151b14ab582a177b004029e
bigbigking <2845758942@qq.com>
2022-06-29 08:56:12 +0800

    feat:taiwindcss

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createHead } from '@vueuse/head'
 import { router } from './router'
+import('./assets/css/index.css')
 const app = createApp(App)
 
 app.use(router)
