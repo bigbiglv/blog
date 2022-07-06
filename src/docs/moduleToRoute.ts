caa1c3dd0e3e267df8a7a5ccfb836040749b622c
bigbigking <2845758942@qq.com>
2022-07-06 10:46:19 +0800

    fix:中文名字的路由刷新获取失败

--- src/docs/moduleToRoute.ts
+++ src/docs/moduleToRoute.ts
@@ -13,10 +13,10 @@ export default function moduleToRoute(modules:Record<string, {[key: string]: any
       
       //父级路由信息
       const name = key.split('/')[2]  //获取到docs下的文件夹名作为的name
-      const path = `/${name}`
+      const path = `/${encodeURI(name)}` //需要转译不然刷新会获取不到路由
       //子级路由信息
       const childName = key.split('/')[3].slice(0,-3)  //截取到文件名
-      const childPath = childName
+      const childPath = encodeURI(childName)
       let index = routes.findIndex(route => route.name === name)
       //父级路由存在
       if(routes.length > 0 && index > -1){
