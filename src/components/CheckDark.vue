f6b95e8df8e83f0740445b387d90354216a9b3bc
bigbigking <2845758942@qq.com>
2022-07-12 14:20:39 +0800

    feat:黑色模式按钮

---
+++ src/components/CheckDark.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import themeStore from '@/store/themeStore'
+import { storeToRefs } from 'pinia';
+const storeTheme = themeStore()
+const { isDark } = storeToRefs(storeTheme)
+
+</script>
+
+<template>
+  <div @click="storeTheme.changeDarkTheme" class="cursor-pointer select-none">
+    {{isDark ? 'Dark' : 'Light'}}
+  </div>
+</template>
--- src/layout/BHeader.vue
+++ src/layout/BHeader.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import SelectCodeThemeVue from '@/components/selectCodeTheme.vue';
+import SelectCodeThemeVue from '@/components/selectCodeTheme.vue'
+import CheckDark from '@/components/CheckDark.vue'
 
 </script>
 
 <template>
   <div class="h-20 w-full flex justify-between items-center ">
     <SelectCodeThemeVue />
+    <CheckDark />
   </div>
 </template>
 
