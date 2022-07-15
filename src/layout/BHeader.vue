1632a34c6bb9aaeea3f73094d1fb8df8089e2ccf
bigbigking <2845758942@qq.com>
2022-07-15 15:37:31 +0800

    feat:title返回首页

--- src/layout/BHeader.vue
+++ src/layout/BHeader.vue
@@ -4,16 +4,23 @@ import CheckDark from '@/components/CheckDark.vue'
 import appStore from '@/store/appStore';
 import { storeToRefs } from 'pinia';
 import useBlogTitle from '@/hooks/useBlogTitle';
+import { useRouter } from 'vue-router';
+const router = useRouter()
 const storeApp = appStore()
 const { mobile } = storeToRefs(storeApp)
 const { title } = useBlogTitle()
+
+function goHome(){
+  router.push('/')
+  storeApp.closeMenu()
+}
 </script>
 
 <template>
   <div class="h-20 w-full flex justify-between items-center px-5 lg:px-0 transition-200 transition-all">
     <div v-if="mobile" @click="storeApp.openMenu()">菜单</div>
     <!-- <SelectCodeThemeVue /> -->
-    <h2>{{title}}</h2>
+    <h2 @click="goHome">{{title}}</h2>
     <CheckDark />
   </div>
 </template>
