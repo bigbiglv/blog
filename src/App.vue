b0ac1d05c1cabe0157b5e05c878dd226392e3cd8
bigbigking <2845758942@qq.com>
2022-08-05 14:31:54 +0800

    feat:ui库的主题切换

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-
+import { darkTheme } from 'naive-ui'
+import themeStore from './store/themeStore.js';
+const storeTheme = themeStore();
 </script>
 
 <template>
-<router-view />
-
+  <n-config-provider :theme="storeTheme.isDark ? darkTheme : ''">
+    <router-view />
+  </n-config-provider>
 </template>
 
 <style>
